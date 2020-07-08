26eb9d7663fbe11dab1ab5ce8d666f9161e51471
Andrew McGrath <amcgrath.wnec@gmail.com>
2020-07-08 09:44:22 -0400

    Update template files

--- readme.markdown
+++ readme.markdown
@@ -1,22 +1,20 @@
 # Rally App Builder
 
-[![Build Status](https://travis-ci.org/RallyApps/rally-app-builder.png?branch=master)](https://travis-ci.org/RallyApps/rally-app-builder)
-
 Rally App Builder is a [Node.js](http://nodejs.org/) command line utility for building apps using the [Rally App SDK](https://help.rallydev.com/apps/2.1/doc/).
 
 ## Installation
 
 Rally App Builder is most easily used when installed globally:
 
-`npm install -g rally-app-builder`
+`npm i -g CustomAgile/rally-app-builder#master`
 
 However, if that does't work (permission errors, etc.) it can be installed locally as well:
 
-`npm install rally-app-builder`
+`npm i CustomAgile/rally-app-builder#master`
 
 ## API
 
-  Usage: rally-app-builder [command] [options]
+  Usage: rab-ca [command] [options]
 
   Commands:
 
@@ -44,16 +42,16 @@ However, if that does't work (permission errors, etc.) it can be installed local
 ## Commands
 
 ### init
-`rally-app-builder init --name=myNewApp`
+`rab-ca init --name=myNewApp`
 Creating a new Rally App is as easy as using init. The init command creates you a  After init creates your App it will automatically run the build command on it for you.
 
 The init command takes a few parameters.  
 *  name : The first is the name for your new App.
-    *  `rally-app-builder init --name=myNewApp`
+    *  `rab-ca init --name=myNewApp`
 *  sdk(optional) : The version of the SDK your App will be created against.
-    *  `rally-app-builder init --name=myNewApp --sdk=2.1`
+    *  `rab-ca init --name=myNewApp --sdk=2.1`
 *  server(optional) : The server you want the debug file to point to. The command below will create a new App using version 2.0 and pointing to the server myownRally.com
-    *  `rally-app-builder init --name=myNewApp --sdk=2.1 --server=https://myOwnRally.com`
+    *  `rab-ca init --name=myNewApp --sdk=2.1 --server=https://myOwnRally.com`
 
 ### build
 
@@ -62,7 +60,7 @@ Run this command before you check your file into source control or whenever you 
 
 The build command can optionally take a templates parameter to use custom html output templates.  Note this is an advanced usage and is generally not necessary unless you are trying to tweak the structure of the generated html output.
 
-`rally-app-builder build --templates=./templates`
+`rab-ca build --templates=./templates`
 
 Also note this parameter can be specified in the config.json file as well.
 
@@ -80,21 +78,21 @@ You can define pre and post build commands to be executed by adding them to your
 
 
 ### run
-`rally-app-builder run`
+`rab-ca run`
 
 The run command starts a local http server and launches your App-debug.html file in the default browser for quick an easy development.
 By default the server listens on port 1337.  This can be changed as follows:
 
-`rally-app-builder run --port=9999`
+`rab-ca run --port=9999`
 
 ### watch
-`rally-app-builder watch [--templates] [--ci]`
+`rab-ca watch [--templates] [--ci]`
 
 The watch command listens for changes to app files and automatically rebuilds the app.
 If the optional `--ci` flag is passed the tests will also be run.
 
 ### test
-`rally-app-builder test [--debug] [--spec]`
+`rab-ca test [--debug] [--spec]`
 
 The test command runs the tests.  By default all tests will be run headlessly.
 If the `--debug` flag is specified the tests will be run in the default browser instead.
--- templates/App.js
+++ templates/App.js
@@ -1,9 +1,161 @@
 Ext.define('CustomApp', {
     extend: 'Rally.app.App',
     componentCls: 'app',
-    launch: function() {
-        //Write app code here
 
-        //API Docs: https://help.rallydev.com/apps/{{sdk_version}}/doc/
+    layout: {
+        type: 'vbox',
+        align: 'stretch'
+    },
+
+    config: {
+        defaultSettings: {},
+    },
+
+    items: [{
+        id: Utils.AncestorPiAppFilter.RENDER_AREA_ID,
+        xtype: 'container',
+        layout: {
+            type: 'hbox',
+            align: 'middle',
+            defaultMargins: 5,
+        }
+    }, {
+        id: Utils.AncestorPiAppFilter.PANEL_RENDER_AREA_ID,
+        xtype: 'container',
+        layout: {
+            type: 'hbox',
+            align: 'middle',
+            defaultMargins: 5,
+        }
+    }, {
+        id: 'mainContainer',
+        xtype: 'container',
+        layout: {
+            type: 'vbox',
+            align: 'stretch',
+            defaultMargins: 5,
+        }
+    }],
+
+    launch: function () {
+        Rally.data.wsapi.Proxy.superclass.timeout = 180000;
+        this.addFilters();
+    },
+
+    addFilters() {
+        this.ancestorFilterPlugin = Ext.create('Utils.AncestorPiAppFilter', {
+            ptype: 'UtilsAncestorPiAppFilter',
+            pluginId: 'ancestorFilterPlugin',
+            settingsConfig: {
+                labelWidth: 225
+            },
+            whiteListFields: ['Tags', 'Milestones', 'c_EnterpriseApprovalEA', 'c_EAEpic', 'DisplayColor'],
+            filtersHidden: false,
+            displayMultiLevelFilter: true,
+            visibleTab: 'HierarchicalRequirement',
+            projectScope: 'user',
+            listeners: {
+                scope: this,
+                ready(plugin) {
+                    plugin.addListener({
+                        scope: this,
+                        select: this.update,
+                        change: this.update
+                    });
+
+                    this.update();
+                },
+            }
+        });
+
+        this.addPlugin(this.ancestorFilterPlugin);
+    },
+
+    async update() {
+        this.setLoading('Loading Filters...');
+        this.down('#mainContainer').removeAll();
+
+        try {
+            let filters = await this.ancestorFilterPlugin.getAllFiltersForType('HierarchicalRequirement', true);
+        } catch (e) {
+            this.showError(e);
+            this.setLoading(false);
+            return;
+        }
+        this.setLoading(false);
+    },
+
+    getData() {
+        this.setLoading('Loading Data...');
+        let context = this.getContext().getDataContext();
+
+        let store = Ext.create('Rally.data.wsapi.Store', {
+            model: '',
+            autoLoad: false,
+            context,
+            filters: [],
+            fetch: ['Name']
+        });
+
+        return this.wrap(store.load());
+    },
+
+    onTimeboxScopeChange(timeboxScope) {
+        this.timeboxScope = timeboxScope;
+        this.callParent(arguments);
+        this.update();
+    },
+
+    showError(msg, defaultMessage) {
+        Rally.ui.notify.Notifier.showError({ message: this.parseError(msg, defaultMessage) });
+    },
+
+    parseError(e, defaultMessage) {
+        defaultMessage = defaultMessage || 'An unknown error has occurred';
+
+        if (typeof e === 'string' && e.length) {
+            return e;
+        }
+        if (e.message && e.message.length) {
+            return e.message;
+        }
+        if (e.exception && e.error && e.error.errors && e.error.errors.length) {
+            if (e.error.errors[0].length) {
+                return e.error.errors[0];
+            } else {
+                if (e.error && e.error.response && e.error.response.status) {
+                    return `${defaultMessage} (Status ${e.error.response.status})`;
+                }
+            }
+        }
+        if (e.exceptions && e.exceptions.length && e.exceptions[0].error) {
+            return e.exceptions[0].error.statusText;
+        }
+        return defaultMessage;
+    },
+
+    async wrap(deferred) {
+        if (!deferred || !_.isFunction(deferred.then)) {
+            return Promise.reject(new Error('Wrap cannot process this type of data into a ECMA promise'));
+        }
+        return new Promise((resolve, reject) => {
+            deferred.then({
+                success(...args) {
+                    resolve(...args);
+                },
+                failure(error) {
+                    Rally.getApp().setLoading(false);
+                    reject(error);
+                }
+            });
+        });
+    },
+
+    getSettingsFields() {
+        return [];
+    },
+
+    setLoading(msg) {
+        this.down('#mainContainer').setLoading(msg);
     }
 });
